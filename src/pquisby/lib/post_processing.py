ece8c89cc349a72cf705c371aaac15b9c6ef4e26
siddardh <sira@redhat27!>
2023-06-13 22:41:55 +0530

    Added class module for both single and compare data to be in single umberella

--- src/pquisby/lib/post_processing.py
+++ src/pquisby/lib/post_processing.py
@@ -21,46 +21,49 @@ class BenchmarkName(enum.Enum):
     SPECJBB = enum.auto()
 
 
-def extract_data(test_name, dataset_name, input_type, data):
-    try:
-        if input_type == InputType.STREAM:
-            csv_data = stream_to_csv(data)
-        elif input_type == InputType.CSV:
-            csv_data = data
-        elif input_type == InputType.OTHER_FILE:
-            with open(data) as csv_file:
-                csv_data = list(csv.reader(csv_file))
-        ret_val = []
-        json_data = {}
-        if test_name == BenchmarkName.UPERF:
-            ret_val, json_data = extract_uperf_data(dataset_name, csv_data)
-        else:
-            pass
-    except Exception as exc:
-        exception_type = type(exc)
-        return {
-            "status": "failed",
-            "exception": str(exc),
-            "exception_type": exception_type,
-        }
-    return {"status": "success", "csv_data": ret_val, "json_data": json_data}
+class QuisbyProcessing:
+    def extract_data(self, test_name, dataset_name, input_type, data):
+        try:
+            if input_type == InputType.STREAM:
+                csv_data = stream_to_csv(data)
+            elif input_type == InputType.CSV:
+                csv_data = data
+            elif input_type == InputType.OTHER_FILE:
+                with open(data) as csv_file:
+                    csv_data = list(csv.reader(csv_file))
+            ret_val = []
+            json_data = {}
+            if test_name == BenchmarkName.UPERF:
+                ret_val, json_data = extract_uperf_data(dataset_name, csv_data)
+            else:
+                pass
+        except Exception as exc:
+            exception_type = type(exc)
+            return {
+                "status": "failed",
+                "exception": str(exc),
+                "exception_type": exception_type,
+            }
+        return {"status": "success", "csv_data": ret_val, "json_data": json_data}
 
-
-def compare_csv_to_json(benchmark_name, input_type, data_stream):
-    result_json = {}
-    flag = 0
-    for dataset_name, data in data_stream.items():
-        json_res = extract_data(benchmark_name, dataset_name, input_type, data)
-        if json_res["json_data"]:
-            json_data = json_res["json_data"]
-        if flag == 0:
-            result_json = json_data
-            flag = flag + 1
-        else:
-            for i in result_json["data"]:
-                metric_unit = i["metrics_unit"]
-                test_name = i["test_name"]
-                for j in json_data["data"]:
-                    if metric_unit == j["metrics_unit"] and test_name == j["test_name"]:
-                        i["instances"].extend(j["instances"])
-    return result_json
+    def compare_csv_to_json(self, benchmark_name, input_type, data_stream):
+        result_json = {}
+        flag = 0
+        for dataset_name, data in data_stream.items():
+            json_res = self.extract_data(benchmark_name, dataset_name, input_type, data)
+            if json_res["json_data"]:
+                json_data = json_res["json_data"]
+            if flag == 0:
+                result_json = json_data
+                flag = flag + 1
+            else:
+                for i in result_json["data"]:
+                    metric_unit = i["metrics_unit"]
+                    test_name = i["test_name"]
+                    for j in json_data["data"]:
+                        if (
+                            metric_unit == j["metrics_unit"]
+                            and test_name == j["test_name"]
+                        ):
+                            i["instances"].extend(j["instances"])
+        return result_json
