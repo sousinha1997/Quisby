1fa461bdd371477dab699a68a613b76695080cca
sousinha97 <sousinha@redhat.com>
2024-01-21 08:19:57 +0530

    auto_hpl fixes

--- quisby/benchmarks/auto_hpl/extract.py
+++ quisby/benchmarks/auto_hpl/extract.py
@@ -12,8 +12,8 @@ def extract_auto_hpl_data(path, system_name):
             file_data = file.readlines()
 
             if len(file_data) > 1:
-                header_row = file_data[0].strip().split(":")
-                data_row = file_data[1].strip().split(":")
+                header_row = file_data[-2].strip().split(":")
+                data_row = file_data[-1].strip().split(":")
 
                 data_dict = {}
                 for key, value in zip(header_row, data_row):
@@ -28,3 +28,5 @@ def extract_auto_hpl_data(path, system_name):
 
             else:
                 return None
+
+extract_auto_hpl_data("/Users/soumyasinha/Workspace/2022/rocky_rhel_gvnic/new_pull_aws/m5.metal/pbench-user-benchmark_dvalin_auto_hpl_test_2022.12.02T09.48.46/hpl-Intel_openblas-2022.12.02-09.48.57.csv","n2-standard-8")
