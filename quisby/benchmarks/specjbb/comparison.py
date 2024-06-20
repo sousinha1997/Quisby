e9159e0cc06c2d147e46e963ad2315b716a644dd
sousinha1997 <sinhasoumya97@gmail.com>
2024-06-20 14:57:50 +0530

    spec

--- quisby/benchmarks/specjbb/comparison.py
+++ quisby/benchmarks/specjbb/comparison.py
@@ -7,9 +7,27 @@ from quisby.sheet.sheet_util import (
     get_sheet,
     create_sheet, clear_sheet_data, clear_sheet_charts,
 )
-from quisby.util import combine_two_array_alternating, merge_lists_alternately
+from quisby.util import combine_two_array_alternating, merge_lists_alternately,read_config
 from quisby.benchmarks.specjbb.graph import graph_specjbb_data
+import re
 
+def extract_prefix_and_number(input_string):
+    match = re.search(r'^(.*?)(\d+)(.*?)$', input_string)
+    if match:
+        prefix = match.group(1)
+        return prefix
+    return None
+
+def compare_inst(item1,item2):
+    cloud_type = read_config("cloud","cloud_type")
+    if cloud_type == "localhost":
+        return True
+    elif cloud_type == "aws":
+        return (item1.split(".")[0] == item2.split(".")[0])
+    elif cloud_type == "gcp":
+        return(item1.split("-")[0],item2.split("-")[0])
+    elif cloud_type == "azure":
+        return(extract_prefix_and_number(item1) == extract_prefix_and_number(item2))
 
 def compare_specjbb_results(spreadsheets, spreadsheetId, test_name, table_name=["Peak", "Peak/$eff"]):
     values = []
@@ -30,7 +48,7 @@ def compare_specjbb_results(spreadsheets, spreadsheetId, test_name, table_name=[
         for ele in list_2:
             if value[0][0] in table_name and ele[0][0] in table_name:
                 if value[0][0] == ele[0][0]:
-                    if value[1][0].split(".")[0] == ele[1][0].split(".")[0]:
+                    if compare_inst(value[1][0],ele[1][0]):
                         results.append([""])
                         for item1 in value:
                             for item2 in ele:
@@ -39,7 +57,7 @@ def compare_specjbb_results(spreadsheets, spreadsheetId, test_name, table_name=[
                         break
 
             elif value[0][0] == "Cost/Hr" and ele[0][0] == "Cost/Hr":
-                if value[1][0].split(".")[0] == ele[1][0].split(".")[0]:
+                if compare_inst(value[1][0],ele[1][0]):
                     results.append([""])
                     for item1 in value:
                         for item2 in ele:
