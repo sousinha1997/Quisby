fa28b9c673a39fcb4fc7df55018967a66735ecd8
sousinha1997 <sinhasoumya97@gmail.com>
2024-07-15 01:29:19 +0530

    speccpu

--- quisby/benchmarks/speccpu/graph.py
+++ quisby/benchmarks/speccpu/graph.py
@@ -41,7 +41,7 @@ def create_series_range_speccpu_compare(column_count, sheetId, start_index, end_
                     "sources": [
                         {
                             "sheetId": sheetId,
-                            "startRowIndex": start_index + 1,
+                            "startRowIndex": start_index,
                             "endRowIndex": end_index,
                             "startColumnIndex": 1,
                             "endColumnIndex": 2,
@@ -58,7 +58,7 @@ def create_series_range_speccpu_compare(column_count, sheetId, start_index, end_
                     "sources": [
                         {
                             "sheetId": sheetId,
-                            "startRowIndex": start_index + 1,
+                            "startRowIndex": start_index ,
                             "endRowIndex": end_index,
                             "startColumnIndex": 2,
                             "endColumnIndex": 3,
@@ -75,7 +75,7 @@ def create_series_range_speccpu_compare(column_count, sheetId, start_index, end_
                     "sources": [
                         {
                             "sheetId": sheetId,
-                            "startRowIndex": start_index + 1,
+                            "startRowIndex": start_index,
                             "endRowIndex": end_index,
                             "startColumnIndex": 3,
                             "endColumnIndex": 4,
@@ -90,7 +90,117 @@ def create_series_range_speccpu_compare(column_count, sheetId, start_index, end_
     return series
 
 
+def compare_spec_data(spreadsheetId, test_name, action):
+    """"""
+    GRAPH_COL_INDEX = 1
+    GRAPH_ROW_INDEX = 0
+    start_index, end_index = None, None
+    sheetId = -1
+    diff_col = [3]
+
+    data = read_sheet(spreadsheetId, test_name)
+    if len(data) > 500:
+        append_empty_row_sheet(spreadsheetId, 3000, test_name)
+
+    for index, row in enumerate(data):
+        if "SYSTEM_NAME" in row:
+            start_index = index
+
+        if start_index:
+            if not row:
+                end_index = index
+            elif index + 1 == len(data):
+                end_index = index + 1
+
+        if end_index:
+            graph_data = data[start_index:end_index]
+            column_count = len(graph_data[1])
+
+            sheetId = get_sheet(spreadsheetId, test_name)["sheets"][0]["properties"][
+                "sheetId"
+            ]
+
+            series = globals()[f'create_series_range_speccpu_{action}'](column_count, sheetId, start_index, end_index)
+
+            requests = {
+                "addChart": {
+                    "chart": {
+                        "spec": {
+                            "title": f"{test_name}",
+                            "subtitle": f"{graph_data[0][0]}",
+                            "basicChart": {
+                                "chartType": "COMBO",
+                                "legendPosition": "BOTTOM_LEGEND",
+                                "axis": [
+                                    {"position": "BOTTOM_AXIS", "title": "Systems"},
+                                    {
+                                        "position": "LEFT_AXIS",
+                                        "title": "Geomean",
+                                    },
+                                    {
+                                        "position": "RIGHT_AXIS",
+                                        "title": "%Diff",
+                                    },
+                                ],
+                                "domains": [
+                                    {
+                                        "domain": {
+                                            "sourceRange": {
+                                                "sources": [
+                                                    {
+                                                        "sheetId": sheetId,
+                                                        "startRowIndex": start_index,
+                                                        "endRowIndex": end_index,
+                                                        "startColumnIndex": 0,
+                                                        "endColumnIndex": 1,
+                                                    }
+                                                ]
+                                            }
+                                        }
+                                    }
+                                ],
+                                "series": series,
+                                "headerCount": 1,
+                            },
+                        },
+                        "position": {
+                            "overlayPosition": {
+                                "anchorCell": {
+                                    "sheetId": sheetId,
+                                    "rowIndex": GRAPH_ROW_INDEX,
+                                    "columnIndex": column_count + GRAPH_COL_INDEX,
+                                }
+                            }
+                        },
+                    }
+                }
+            }
+            if GRAPH_COL_INDEX >= 5:
+                GRAPH_ROW_INDEX += 20
+                GRAPH_COL_INDEX = 1
+            else:
+                GRAPH_COL_INDEX += 6
+
+            body = {"requests": requests}
+
+            sheet.batchUpdate(spreadsheetId=spreadsheetId, body=body).execute()
+
+            start_index, end_index = None, None
+
+            time.sleep(3)
+    if sheetId != -1:
+        threshold = read_value("percent_threshold", test_name)
+        if not threshold:
+            threshold = "5"
+        for col in diff_col:
+            update_conditional_formatting(spreadsheetId, sheetId, col, threshold)
+
+
 def graph_speccpu_data(spreadsheetId, test_name, action):
+    if action == "compare":
+        compare_spec_data(spreadsheetId, test_name, action)
+        return
+
     """"""
     GRAPH_COL_INDEX = 1
     GRAPH_ROW_INDEX = 0
